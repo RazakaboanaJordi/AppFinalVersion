db51d463111b0709c809076781fa39c5ed08cc9c
Freezaa9 <geoffreyharrazi@gmail.com>
2017-12-08 02:38:33 +0100

    j

--- src/mocks/providers/items.ts
+++ src/mocks/providers/items.ts
@@ -16,38 +16,50 @@ export class Items {
   constructor() {
     let items = [
       {
-        "name": "Burt Bear",
-        "profilePic": "assets/img/speakers/bear.jpg",
-        "about": "Burt is a Bear."
+        "name": "Alcool",
+        "profilePic": "assets/img/speakers/1.jpg",
+        "about": "Désormais c’est 0,2 g d'alcool par litre de sang
+Désormais, le taux d’alcool autorisé est 0,2 g/l pour tous les permis probatoires. La réglementation s’applique à tous les jeunes conducteurs pendant : 
+
+3 ans après l’obtention du permis, la perte de 12 points ou l'annulation de son permis
+ 2 ans si le permis a été obtenu dans le cadre de la conduite accompagnée.
+0.2 g/l, c'est zéro verre d'alcool car dès le 1er verre, ce seuil peut être dépassé.
+
+Quelles sont les sanctions ?
+Les jeunes conducteurs contrôlés avec un taux d’alcoolémie supérieur ou égal à 0,2 g/l encourent : 
+
+un retrait de 6 points sur leur permis.
+une amende forfaitaire de 135 €.
+une immobilisation du véhicule."
       },
       {
         "name": "Charlie Cheetah",
-        "profilePic": "assets/img/speakers/cheetah.jpg",
+        "profilePic": "assets/img/speakers/2.jpg",
         "about": "Charlie is a Cheetah."
       },
       {
         "name": "Donald Duck",
-        "profilePic": "assets/img/speakers/duck.jpg",
+        "profilePic": "assets/img/speakers/3.jpg",
         "about": "Donald is a Duck."
       },
       {
         "name": "Eva Eagle",
-        "profilePic": "assets/img/speakers/eagle.jpg",
+        "profilePic": "assets/img/speakers/4.jpg",
         "about": "Eva is an Eagle."
       },
       {
         "name": "Ellie Elephant",
-        "profilePic": "assets/img/speakers/elephant.jpg",
+        "profilePic": "assets/img/speakers/5.jpg",
         "about": "Ellie is an Elephant."
       },
       {
         "name": "Molly Mouse",
-        "profilePic": "assets/img/speakers/mouse.jpg",
+        "profilePic": "assets/img/speakers/6.jpg",
         "about": "Molly is a Mouse."
       },
       {
         "name": "Paul Puppy",
-        "profilePic": "assets/img/speakers/puppy.jpg",
+        "profilePic": "assets/img/speakers/7.jpg",
         "about": "Paul is a Puppy."
       }
     ];
